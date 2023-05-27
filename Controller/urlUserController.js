49255c610d1bcbd203207091595ab08b3449fad4
VigneshThanigachalam <vigneshthanika03@gmail.com>
2023-05-27 13:09:19 +0530

    changed subject in mail

--- Controller/urlUserController.js
+++ Controller/urlUserController.js
@@ -119,7 +119,7 @@ export const forgetPasswordtoken = asyncHandler(async (req, res) => {
         const resetURL = `<a href="${appUrl}/reset-password/${token}">click here</a>`;
         const data = {
             to: email,
-            subject: "Dress Color Sugesstion App - Reset Password Link",
+            subject: "Trim URL App - Reset Password Link",
             html: `Please ${resetURL} to create new password.`,
         };
         sendEmail(data);
